e89ee81caab37be29b464aa605fec05f9a67a2e1
ruchit-patel <Itstartswithme123>
2017-04-18 22:57:06 +0530

    insert page escape sequence

--- pro.php
+++ pro.php
@@ -1,17 +1,20 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "division";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$nm = mysqli_real_escape_string($conn,$_POST['name']);
+    $cat = mysqli_real_escape_string($conn,$_POST['category']);
+    $des = mysqli_real_escape_string($conn,$_POST['des']);
+  //$nm=$_POST['name'];
+  //$cat=$_POST['category'];
+  //$des=$_POST['des'];
 
-  $nm=$_POST['name'];
-  $cat=$_POST['category'];
-  $des=$_POST['des'];
-  $servername = "localhost";
-  $username = "root";
-  $password = "admin";
-  $dbname = "division";
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-  // Check connection
-  if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-  }
 
   $sql = "INSERT INTO Talents values ('$nm','$cat','$des')";
 
