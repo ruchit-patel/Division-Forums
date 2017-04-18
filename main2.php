0dd39dafc3860ea81aa13784b03e19f9509ae146
scorpionakki <alkesh302@gmail.com>
2017-04-18 22:32:03 +0530

    added perks table

---
+++ main2.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Insert</title></head>
+  <body>
+<form action="pro2.php" method="post">
+  Name:<input type="text" name="name" required><br><br>
+  Category: <input type="text" name="category" required><br><br>
+  Description:<input type="text" name="des" required><br><br>
+  <input type="submit">
+</form>
+  </body>
+</html>
---
+++ pro2.php
@@ -0,0 +1,27 @@
+<?php
+
+  $nm=$_POST['name'];
+  $cat=$_POST['category'];
+  $des=$_POST['des'];
+  $servername = "localhost";
+  $username = "root";
+  $password = "admin";
+  $dbname = "division";
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $sql = "INSERT INTO Perks values ('$nm','$cat','$des')";
+
+  if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+       header("location: main.html");
+  } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+
+?>
