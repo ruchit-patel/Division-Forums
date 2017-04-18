0811f87a79a2ce23e20da6db5ce64addfe5a1c13
ruchit-patel <Itstartswithme123>
2017-04-18 22:13:52 +0530

    insert page

---
+++ pro.php
@@ -0,0 +1,27 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=="POST"){
+  $nm=$_POST['name'];
+  $cat=$_POST['category'];
+  $des=$_POST['des'];
+  $servername = "localhost";
+  $username = "root";
+  $password = "admin";
+  $dbname = "division";
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $sql = "INSERT INTO Talents values(Name,Category,Description) VALUES ('$nm','$cat','$des')";
+
+  if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+       header("location: main.html");
+  } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+  }
+?>
