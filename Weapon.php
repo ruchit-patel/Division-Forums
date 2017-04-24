471204473bf17ec46ab7c1dc599d0eca61a4d47e
Amorpheuz <mail2ypd@gmail.com>
2017-04-24 14:48:16 +0530

    Added CSS for weps

--- Weapon.php
+++ Weapon.php
@@ -1,8 +1,17 @@
 <html>
     <head>
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="CSS\wep.css">
     </head>
     <body>
-
+        
     </body>
 </html>
