1d81e17b8b0858c87c331150256a12c107ccfc17
ruchit-patel <Itstartswithme123>
2017-04-25 23:00:27 +0530

    did insert page

--- insafter.php
+++ insafter.php
@@ -8,11 +8,84 @@ include 'select.php';
       <div class="card">
         <div class="card-block">
 <?php
-echo '<form action="techpro.php" method="post">
+if($_SERVER['REQUEST_METHOD']=="GET"){
+  if($_GET['table']=="Medical"){
+echo '<br><br><form action="techpro.php" method="post">
   Description:<input type="text" name="des" required><br><br>
   Modifiaction: <input type="text" name="mod" required><br><br>
   ID: <input type="text" name="id"><br><br>
-  <input type="submit">';?>
+  <input type="submit">';
+}
+elseif ($_GET['table']=="News") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="Perks") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="Security") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="Skills") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="Talents") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="Technicals") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+
+elseif ($_GET['table']=="wepBns") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="wepMain") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+elseif ($_GET['table']=="wepStats") {
+  echo '<br><br><form action="techpro.php" method="post">
+    Description stats:<input type="text" name="des" required><br><br>
+    Modifiaction: <input type="text" name="mod" required><br><br>
+    ID: <input type="text" name="id"><br><br>
+    <input type="submit">';
+}
+}
+
+
+
+
+  ?>
 </form></div></div></div>
   <div class="col-md-3"></div>
 </div>
