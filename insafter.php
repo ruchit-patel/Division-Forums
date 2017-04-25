dfa87958e8ca765bfb8ee7f7b3f5c479e275b26b
ruchit-patel <Itstartswithme123>
2017-04-25 22:48:28 +0530

    try

--- insafter.php
+++ insafter.php
@@ -1,12 +1,18 @@
 <?php
 include 'select.php';
  ?>
+<div class="row">
+  <div class="col-md-3">
+  </div>
+    <div class="col-md-6">
 <form action="techpro.php" method="post">
   Description:<input type="text" name="des" required><br><br>
   Modifiaction: <input type="text" name="mod" required><br><br>
   ID: <input type="text" name="id"><br><br>
   <input type="submit">
-</form>
+</form></div>
+  <div class="col-md-3"></div>
+</div>
 
 
 ?>
