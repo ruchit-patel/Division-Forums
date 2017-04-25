c39ecc3201e2ff9bcb818c5d73705be45f581b8f
ruchit-patel <Itstartswithme123>
2017-04-25 22:39:29 +0530

    trying to insert

--- select.php
+++ select.php
@@ -21,8 +21,14 @@
           <center><p class="crd-h1">Select The name of the table</p></center>
           <div class="container">
             <div class="dropdown"><form action="insafter.php" method="get">
+<<<<<<< Updated upstream
 
                     <select  id="sel1" name="table">
+=======
+          <button class="btn btn-warning dropdown-toggle" type="button" data-toggle="dropdown">Select Table
+                  <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+>>>>>>> Stashed changes
                       <?php
                       include 'tabinfo.php';
                       ?>
