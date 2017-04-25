313c2254773c38643216fd6cb1784637ec1c5389
ruchit-patel <Itstartswithme123>
2017-04-25 22:53:08 +0530

    try

--- insafter.php
+++ insafter.php
@@ -8,11 +8,11 @@ include 'select.php';
       <div class="card">
         <div class="card-block">
 <?php
-echo "<form action="techpro.php" method="post">
+echo '<form action="techpro.php" method="post">
   Description:<input type="text" name="des" required><br><br>
   Modifiaction: <input type="text" name="mod" required><br><br>
   ID: <input type="text" name="id"><br><br>
-  <input type="submit">";?>
+  <input type="submit">';?>
 </form></div></div></div>
   <div class="col-md-3"></div>
 </div>
