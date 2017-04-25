348b1b54b446bc71520713252979252df4099b56
ruchit-patel <Itstartswithme123>
2017-04-25 21:43:08 +0530

    removed a tag

--- tabinfo.php
+++ tabinfo.php
@@ -15,8 +15,8 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo '<li><a href="">'.$row["Tables_in_division"].'</a></li>';
-      //  echo '<li><a href="#">CSS</a></li>';
+        echo '<li>'.$row["Tables_in_division"].'</li>';
+
         }
 } else {
     echo "0 results";
