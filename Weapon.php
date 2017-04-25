dda520bd81505717d4a6dbeae8830a53047e26d5
Amorpheuz <mail2ypd@gmail.com>
2017-04-25 19:54:57 +0530

    Further Changes

--- Weapon.php
+++ Weapon.php
@@ -6,6 +6,11 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="CSS\wep.css">
+        <style>
+            .card{
+                min-height: 155px;
+            }
+        </style>
     </head>
     <body class="parallax1">
         <div class="wrapper">
@@ -21,6 +26,21 @@
                 <div class="col-md-4">
                     <div class="card">
                         <img src="Images\WepImgs\M249-B.jpg" style="width:100%;">
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top:5px;">
+                <div class="col-md-6 col-md-offset-1">
+                    <div class="card">
+                        <div class="card-block">
+                            <p class="crd-h1">Some Real World Trivia</p>
+                            The M249 light machine gun (LMG), formerly designated the M249 Squad Automatic Weapon (SAW), and formally written as Light Machine Gun, 5.56 mm, or the .223 military rnd. The M249, is the American adaptation of the Belgian FN Minimi, a light machine gun manufactured by the Belgian company FN Herstal (FN). The M249 is manufactured in the United States by the local subsidiary FN Manufacturing LLC in South Carolina and is widely used in the U.S. Armed Forces. The weapon was introduced in 1984 after being judged the most effective of a number of candidate weapons to address the lack of automatic firepower in small units. The M249 provides infantry squads with the heavy volume of fire of a machine gun combined with accuracy and portability approaching that of a rifle.
+                        </div>
+                    </div>
+                </div>  
+                <div class="col-md-4">
+                    <div class="card">
+                        
                     </div>
                 </div>
             </div>
