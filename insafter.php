5a99fd3aa5052b8988db35d830f127f2f6b1b6cd
ruchit-patel <Itstartswithme123>
2017-04-25 22:45:21 +0530

    try

--- insafter.php
+++ insafter.php
@@ -1,10 +1,12 @@
 <?php
-echo "<form action="techpro.php" method="post">
+include 'select.php';
+ ?>
+<form action="techpro.php" method="post">
   Description:<input type="text" name="des" required><br><br>
   Modifiaction: <input type="text" name="mod" required><br><br>
   ID: <input type="text" name="id"><br><br>
   <input type="submit">
-</form>";
+</form>
 
 
 ?>
