7fed70c7faf1f04b189783824eac4c0e51226656
ruchit-patel <Itstartswithme123>
2017-04-26 12:43:10 +0530

    made changed to select.php

--- tabinfo.php
+++ tabinfo.php
@@ -15,7 +15,16 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo '<option>'.$row["Tables_in_division"].'</option>';
+      if($row["Tables_in_division"]=="wepBns" || $row["Tables_in_division"]=="wepMain" || $row["Tables_in_division"]=="wepStats" ){
+        echo '<option> Weapons </option>';
+      }
+        else if($row["Tables_in_division"]=="Perks" || $row["Tables_in_division"]=="Security" || $row["Tables_in_division"]=="Technical"){
+          echo '<option> Skills </option>';
+        }
+        else{
+          echo "<option> ".$row["Tables_in_division"]."</option>";
+        }
+
         }
 } else {
     echo "0 results";
