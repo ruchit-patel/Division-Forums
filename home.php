cbca9e556dd164ef325331fa0f7034b7536512f5
ruchit-patel <Itstartswithme123>
2017-04-28 00:44:39 +0530

    made the slide show from auto scrolling

--- home.php
+++ home.php
@@ -12,7 +12,8 @@
 
 <div class="container">
   <h2>Carousel Example</h2>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <div id="myCarousel" class="carousel slide" data-interval="false">
+  <!--div id="myCarousel" class="carousel slide" data-ride="carousel"-->
     <!-- Indicators -->
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
