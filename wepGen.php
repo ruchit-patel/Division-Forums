d1b3bf312ee7454156b951005a620ca9d3d26a40
Amorpheuz <mail2ypd@gmail.com>
2017-04-27 22:55:28 +0530

    Weapon changes

---
+++ wepGen.php
@@ -0,0 +1,36 @@
+<html>
+    <head>   
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="CSS\wep.css">
+        <style>
+            .card{
+                min-height: 155px;
+            }
+        </style>
+    </head>
+    <body class="parallax1">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-1">
+                    <div class="card">
+                        <div class="card-block">
+                            <p class="crd-h1">WEAPONS</p>
+                                Weapons in Tom Clancy's The Division are separated into 6 unique classes. They are:<br><br>
+                                <ul>
+                                    <li>Shotguns</li>
+                                    <li>Submachine Guns(SMG)</li>
+                                    <li>Assault Rifles</li>
+                                    <li>Marksman Rifles</li>
+                                    <li>Light Machine Guns</li>
+                                    <li>Sidearms</li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
