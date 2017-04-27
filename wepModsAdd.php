1505967bd2aaf018696488e9dc74dea05c057e0c
Amorpheuz <mail2ypd@gmail.com>
2017-04-27 18:59:29 +0530

    Added wepMods gui and script

---
+++ wepModsAdd.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <form method="post" action="wepTalAddq.php">
+            ID:<input type="text" name="wid"><br><br>
+            Optics_sm:<input type="checkbox" name="mos" value="T"><br><br>
+            Optics_lg:<input type="checkbox" name="mol" value="T"><br><br>
+            Und_sm:<input type="checkbox" name="mus" value="T"><br><br>
+            Und_lg:<input type="checkbox" name="mul" value="T"><br><br>
+            Mag:<input type="checkbox" name="mmg" value="T"><br><br>
+            Muz_sm:<input type="checkbox" name="mms" value="T"><br><br>
+            Muz_lg:<input type="checkbox" name="mml" value="T"><br><br>
+            <input type="Submit">
+        </form>
+    </body>
+</html>
