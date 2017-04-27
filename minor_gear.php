6b3d922a55e0de28a3d9c5f3ce2ea9b13ca3830a
ruchit-patel <Itstartswithme123>
2017-04-28 00:07:13 +0530

    had a mistake

--- minor_gear.php
+++ minor_gear.php
@@ -34,7 +34,7 @@ $bleedresmax=mysqli_real_escape_string($conn,$_POST['bleedresmax']);
 $killxpmin=mysqli_real_escape_string($conn,$_POST['killxpmin']);
 $killxpmax=mysqli_real_escape_string($conn,$_POST['killxpmax']);
 
-$sql = "INSERT INTO Minor_Attr (Dmg_vs_ElitesMin, Dmg_vs_ElitesMax, Shock_resMin , Shock_resMax,Burn_resMin ,Burn_resMax, Disorient_resMin, Disorient_resMax,Blind_Deaf_resMin ,Blind_Deaf_resMax,Disrupt_resMin ,Disrupt_resMax,Bleed_resMin ,Bleed_resMax,Kill_xpMin,Kill_xpMax,gear_id)values ($dvemin','$dvemax','$shock_min','$shock_max','$burn_res_min','$burn_res_max','$dermin','$dermax','$bdres_min','$bdres_max','$disrupt_min','$disrupt_max','$bleedresmin','$bleedresmax','$killxpmin','$killxpmax','$id')";
+$sql = "INSERT INTO Minor_Attr (Dmg_vs_ElitesMin, Dmg_vs_ElitesMax, Shock_resMin , Shock_resMax,Burn_resMin ,Burn_resMax, Disorient_resMin, Disorient_resMax,Blind_Deaf_resMin ,Blind_Deaf_resMax,Disrupt_resMin ,Disrupt_resMax,Bleed_resMin ,Bleed_resMax,Kill_xpMin,Kill_xpMax,gear_id)values ('$dvemin','$dvemax','$shock_min','$shock_max','$burn_res_min','$burn_res_max','$dermin','$dermax','$bdres_min','$bdres_max','$disrupt_min','$disrupt_max','$bleedresmin','$bleedresmax','$killxpmin','$killxpmax','$id')";
 
 
 if (mysqli_query($conn, $sql)) {
