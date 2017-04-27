b5527a51d811a16d2633e9ba17c38eae93c34d8a
ruchit-patel <Itstartswithme123>
2017-04-27 19:39:08 +0530

    made a correction

--- skill_desc.php
+++ skill_desc.php
@@ -15,14 +15,15 @@ if($_SERVER['REQUEST_METHOD']=="POST")
 {
 	$skill_description=$_POST['skill_description'];
 	$image_path=$_POST['image_path'];
+  $id=$_POST['id'];
 }
 
-for($i=1;$i<13;$i++)
-{
-		$q="INSERT INTO Skills (Skill_Description,Image) values('$skill_description','$image_path') where Skill_Id='$i'";
-
+//for($i=1;$i<13;$i++)
+//{
+		//$q="INSERT INTO Skills (Skill_Description,Image) values('$skill_description','$image_path') where Skill_Id='$id'";
+    $q="UPDATE Skills set Skill_Description='$skill_description', Image='$image_path' where $id='$id'";
 		mysqli_query($conn,$q);
-}
+//}
 
 mysqli_close($conn);
 ?>
