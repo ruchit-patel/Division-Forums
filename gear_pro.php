d27f3b7e4dff0fd5d82fcd3442e896fb90e28bc4
ruchit-patel <Itstartswithme123>
2017-04-27 20:26:02 +0530

    added gear insert page

---
+++ gear_pro.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "division";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$id=mysqli_real_escape_string($conn,$_POST['id']);
+$type=mysqli_real_escape_string($conn,$_POST['type']);
+$mnst=mysqli_real_escape_string($conn,$_POST['msq']);
+$noma1=mysqli_real_escape_string($conn,$_POST['noma1']);
+$noma2=mysqli_real_escape_string($conn,$_POST['noma2']);
+$secstat=mysqli_real_escape_string($conn,$_POST['secnst']);
+$mnstmin=mysqli_real_escape_string($conn,$_POST['mnstmin']);
+$mnstmax=mysqli_real_escape_string($conn,$_POST['mnstmax']);
+$natarm=mysqli_real_escape_string($conn,$_POST['natarm']);
+
+$sql = "INSERT INTO gearMain values ('$id','$type','$mnst','$noma1','$noma2','$secstat','$mnstmin','$mnstmax','$natarm')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+     header("location: gear_ins.html");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
