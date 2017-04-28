a4b707afa46cb12fe7323f63c71d53d5a1ff3114
ruchit-patel <Itstartswithme123>
2017-04-28 13:53:06 +0530

    blah

--- home.php
+++ home.php
@@ -95,7 +95,7 @@
   <!--/div-->
   <div class="col-md-1"></div>
   <div class="col-md-2">
-          <div class="card">
+          <div class="card" style="height:100px;">
           <div class="card-block">
 
             <p class="crd-h1" style="font-size:30px;">News</p>
