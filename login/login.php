128862dc509e6811fe01455128524fbf92098941
ruchit-patel <Itstartswithme123>
2017-04-29 04:51:40 +0530

    blah

--- login/login.php
+++ login/login.php
@@ -11,6 +11,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $username = "root";
     $password = "admin";
     $dbname = "division";
+
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
     if (!$conn) {
@@ -18,14 +19,13 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     }
         $id = mysqli_real_escape_string($conn,$_POST['uname']);
         $pass = mysqli_real_escape_string($conn,$_POST['pass']);
-<<<<<<< HEAD
+
         $retrive="SELECT Acc_Name,Password from Account where Acc_Name='$id' AND  Password=SHA1('$pass')";
-=======
-        $retrive="SELECT Acc_Name,Password from Account where Acc_Name='$id' AND  Password='$pass'";
->>>>>>> 63e24af4c2798f2f9c6f262c1c9920b1f471b589
+
         $run=@mysqli_query($conn,$retrive);
         if(mysqli_num_rows($run)==1){
           $row=mysqli_fetch_array($run,MYSQLI_ASSOC);
+          session_start();
           $_SESSION['uname']=$uname;
           echo "Hello".$_SESSION['uname'];
 
