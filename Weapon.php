61afe99c78039d9613ded0b24c475d0fcad2f9a5
Amorpheuz <mail2ypd@gmail.com>
2017-04-29 02:06:06 +0530

    test

--- Weapon.php
+++ Weapon.php
@@ -125,7 +125,7 @@
                             echo "<div class=\"card\">";
                                 echo "<div class=\"card-block\">";
                                     echo "<p class=\"crd-h1\">Stats</p>";
-                                    echo "<table class=\"statTbl\" cellpadding=\"10\" border=\"1\" style='table-layout: fixed; text-align:center;width:100%;'>";
+                                    echo "<table class=\"statTbl\" cellpadding=\"10\" border=\"1\" style='table-layout: fixed; text-align:center;width:100%;font-size: 18px;'>";
                                         echo "<tr><td>Weapon Type</td><td>RPM</td><td>Magazine Size</td><td>Optimal Range</td><td>Reload Speed</td><td>Headshot Multiplier</td></tr>";
                                         echo "<tr><td>$wType</td><td>$wRPM</td><td>$wMgsi</td><td>$wRng</td><td>$wRld</td><td>$wHsd</td></tr>";
                                     echo "</table>";
