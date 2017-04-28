db6923e45c38f040a2460df0f4511fbb9f5b6bb5
Amorpheuz <mail2ypd@gmail.com>
2017-04-28 09:24:30 +0530

    Further changes to wep part

--- wepGen.php
+++ wepGen.php
@@ -19,14 +19,24 @@
                     <div class="card">
                         <div class="card-block">
                             <p class="crd-h1">WEAPONS</p>
-                                Weapons in Tom Clancy's The Division are separated into 6 unique classes. They are:<br><br>
+                                <p>Weapons in Tom Clancy's The Division are separated into 6 unique classes. They are:</p>
                                 <ul>
-                                    <li>Shotguns</li>
-                                    <li>Submachine Guns(SMG)</li>
-                                    <li>Assault Rifles</li>
-                                    <li>Marksman Rifles</li>
-                                    <li>Light Machine Guns</li>
-                                    <li>Sidearms</li>
+                                    <li><a href="#">Shotguns</a></li>
+                                    <li><a href="#">Submachine Guns</a></li>
+                                    <li><a href="#">Assault Rifles</a></li>
+                                    <li><a href="#">Marksman Rifles</a></li>
+                                    <li><a href="#">Light Machine Guns</a></li>
+                                    <li><a href="#">Sidearms</a></li>
+                                </ul>
+                                <p>Players are able to customize their weapons to make them unique and personal to their agent. Guns can be customized with a variety of attachments which are referred to as modifications, such as scopes, grips, sights, magazines, muzzle attachments and weapon skins. Weapons also have a different rarity, Such as:</p>
+                                <ul>
+                                    <li><l style="color: #9E9E9E;">Worn</l></li>
+                                    <li><l style="color: #8BC34A;">Standard</l></li>
+                                    <li><l style="color: #1E88E5;">Specialized</l></li>
+                                    <li><l style="color: #9C27B0;">Superior</l></li>
+                                    <li><l style="color: #FFD600;">High-end</l></li>
+                                    <li><l style="color: #D32F2F;">Exotic</l></li> 
+                                </ul>
                         </div>
                     </div>
                 </div>
