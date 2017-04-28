bbab0b1c17f2713ce69538934e55e8029a7e5f89
scorpionakki <alkesh302@gmail.com>
2017-04-29 05:25:43 +0530

    kevu

--- extra.php
+++ extra.php
@@ -22,22 +22,23 @@
 			}	
 			for($i=01;$i<12;$i++)
 			{	
-			$string="SELET wep_Img FROM Extra where wep_ID="WP00$i"";
+			$string="SELET wep_Img FROM Extra where wep_ID=\"WP00$i\"";
 			//$char="s";
 			//$attachment="\\";	
 			
 			if(mysqli_query($conn,$string))
 			{
 					echo "Blah";
+					$newstring = addcslashes($string,"s");
+			
+					echo "$string";
 			}
 			else
 			{
 					echo "Error";
 			}		
 			
-			$newstring = addcslashes($string,"s");
 			
-			echo "$string";
 			}
 			
 ?>
