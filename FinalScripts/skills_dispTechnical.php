3068e06055a29765acb66b56f0722140d1ceb3d2
scorpionakki <alkesh302@gmail.com>
2017-04-29 03:07:25 +0530

    blah

--- FinalScripts/skills_dispTechnical.php
+++ FinalScripts/skills_dispTechnical.php
@@ -16,8 +16,9 @@
                 while($row = mysqli_fetch_assoc($result)){
                     $skill_id=$row['Skill_id'];
                     $name=$row['Name'];
-                    echo "<td><form action='' method='POST' id='wepDecide'>";
+                    echo "<td><form action='skills_PHP.php' method='POST' id='wepDecide'>";
                     echo "<input type='hidden' name='skill_id' value='$skill_id'>";
+					echo "<input type='hidden' name='category_type' value='Tech Skills'>";
                     echo "<input type='Submit' class='caserTech' value='$name' form='wepDecide'>";
                     echo "</form></td>";
                     if($i%3==0){
