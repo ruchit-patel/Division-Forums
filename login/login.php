9c6c9f551fe5843870bef084bb4b54acf383de1a
ruchit-patel <Itstartswithme123>
2017-04-29 04:36:44 +0530

    login

---
+++ login/login.php
@@ -0,0 +1,32 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=="POST"){
+  if(empty($_POST['uname'])){
+    echo "Enter a User Name";
+  }
+  else if(empty($_POST['pass'])){
+    echo "Enter a password";
+  }
+  else {
+    $servername = "localhost";
+    $username = "root";
+    $password = "admin";
+    $dbname = "division";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+        $id = mysqli_real_escape_string($conn,$_POST['uname']);
+        $pass = mysqli_real_escape_string($conn,$_POST['pass']);
+        $retrive="SELECT Acc_Name,Password from Account where Acc_Name='$id' AND  Password=sha1('$pass')";
+        $run=@mysqli_query($conn,$retrive);
+        if(mysqli_num_rows($run)==1){
+          $row=mysqli_fetch_array($run,MYSQLI_ASSOC);
+          $_SESSION['uname']=$uname;
+          echo "Hello".$_SESSION['uname'];
+
+        }
+  }
+
+}
+?>
