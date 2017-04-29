b76bc4f3bb9a9b57563ca41a5148f9a072254a01
ruchit-patel <Itstartswithme123>
2017-04-29 12:16:28 +0530

    yolo

--- logout.php
+++ logout.php
@@ -5,5 +5,5 @@ echo $_SESSION['uname'];
 echo "<br> Session destroyed! Good Bye!";
 echo "Login again to continue.";
 session_destroy();
-echo "<a href=\"login/index.html\"><button>Login Again</button></a>";
+echo "<a href=\"login/index.php\"><button>Login Again</button></a>";
  ?>
