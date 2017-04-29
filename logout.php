3866a8edf6f3c67922bfecf2ee0053203a0f36f9
ruchit-patel <Itstartswithme123>
2017-04-29 09:45:18 +0530

    test

--- logout.php
+++ logout.php
@@ -5,4 +5,5 @@ echo $_SESSION['uname'];
 echo "<br> Session destroyed! Good Bye!";
 echo "Login again to continue.";
 session_destroy();
+echo "<button href=/"login/index.html/">Login Again</button>";
  ?>
