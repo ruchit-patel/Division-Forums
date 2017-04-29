002464e4f6e3821c5681f6e01194bafbcbef8798
Amorpheuz <mail2ypd@gmail.com>
2017-04-29 09:29:19 +0530

    Test

--- updater.php
+++ updater.php
@@ -1,13 +1,14 @@
 <?php
     require 'update.php';
 ?>
-<div class="row">
+<div class="row" style="margi-top:15px;">
     <div class="col-md-3">
     </div>
     <div class="col-md-6">
         <div class="card">
             <div class="card-block">
                 <?php
+                    ini_set('display_errors',1);
                     if($_SERVER['REQUEST_METHOD']=='GET'){
                         $tbl=$_GET['table'];
                         if($tbl=='Weapons'){
