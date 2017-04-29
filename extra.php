d7c6107ae053ed60238f2aebac2a5749d3abd7f3
scorpionakki <alkesh302@gmail.com>
2017-04-29 06:04:06 +0530

    keuv

--- extra.php
+++ extra.php
@@ -1,52 +1,35 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>General Info Card</title>
-
-
-</head>
-
 <body>
 
-
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "division";
 
-			ini_set('display_errors',1);
-			$servername = "localhost";
-            $username = "root";
-            $password = "admin";
-            $dbname = "division";
-            $conn = mysqli_connect($servername, $username, $password, $dbname);
-            // Check connection
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-			}	
-			/*for($i=01;$i<12;$i++)
-			{	
-			$string="SELET wep_Img FROM Extra where wep_ID=\"WP00$i\""; */
-			//$char="s";
-			//$attachment="\\";	
-			
-			$result = mysqli_query('SELECT wep_Img FROM Extra where wep_ID=\"WP00$i\"');
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
 
-while ($row = mysqli_fetch_assoc($result)){
-     $rowslashes=addcslashes($row,"s"); //this will go for every result, so every row in the database
-	 echo $rowslashes;
+$sql = "SELECT wep_Img FROM Extra";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         addcslashes($row,"s");
+		 return $row;
+     }
+} else {
+     echo "0 results";
 }
-			
-			/* $result=mysqli_query($conn,$string)
-			$row=mysqli_fetch_assoc($result);
-			$arr=array();
-			for($i=0;$i<$row.length();$i++){
-				foreach()
-			$arr[$i]=$row['wep_Img'];
-			
-			} */
-				
-			
-			
-			
-			
-?>
+
+$conn->close();
+?>  
 
 </body>
 </html>
