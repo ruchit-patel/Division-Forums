36346bbc0d388c5c9d0a479c3c4e356996d0c699
Amorpheuz <mail2ypd@gmail.com>
2017-04-29 09:17:04 +0530

    Changes

--- FinalScripts/dispTals.php
+++ FinalScripts/dispTals.php
@@ -14,9 +14,10 @@
                 $i=1;
                 $result = mysqli_query($conn, $qur) OR die("".mysqli_error($conn));
                 while($row = mysqli_fetch_assoc($result)){
-                    $tNm=$row['wep_Name'];
-                    echo "<input type='hidden' name='tNm' value='$'>";
-                    echo "<input type='Submit' class='caser' value='$wName'>";
+                    $tNm=$row['Name'];
+                    echo "<td><form action=\"Weapon.php\" name=\"talList\" method=\"GET\">";
+                    echo "<input type='hidden' name='wID' value='$tNm'>";
+                    echo "<input type='Submit' class='caser' value='$tNm'>";
                     echo "</form></td>";
                     if($i%3==0){
                         echo "</tr>";
