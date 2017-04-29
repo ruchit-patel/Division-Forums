5e0c64d075728181c4e044590773ed94cccb27c8
scorpionakki <alkesh302@gmail.com>
2017-04-29 06:06:41 +0530

    blah

--- extra.php
+++ extra.php
@@ -21,8 +21,8 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         addcslashes($row,"s");
-		 return $row;
+        $slashes=addcslashes($row,"s");
+		 return $slashes;
      }
 } else {
      echo "0 results";
