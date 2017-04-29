0ad5917e6e7803ff591b480be26e46150a431ed3
ruchit-patel <Itstartswithme123>
2017-04-29 07:13:44 +0530

    sign up added

---
+++ login/signup.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "division";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+    $unm = mysqli_real_escape_string($conn,$_POST['unm']);
+    $pass = mysqli_real_escape_string($conn,$_POST['pass1']);
+    $fname = mysqli_real_escape_string($conn,$_POST['fname']);
+
+  $sql = "INSERT INTO Account (Acc_Name, Password,Full_Name) values ('$unm','$pass','$fname')";
+
+  if (mysqli_query($conn, $sql)) {
+    session_start();
+    $_SESSION['name']==$unm;
+       header("location: home.php");
+  } else {
+      echo "The username already exists!";
+  }
+
+  mysqli_close($conn);
+
+?>
