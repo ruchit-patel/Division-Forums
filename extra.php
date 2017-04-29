e8a798e25390c14cc08fbd0a93170fcd8af0097d
scorpionakki <alkesh302@gmail.com>
2017-04-29 06:14:19 +0530

    kevu

--- extra.php
+++ extra.php
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
         $row['wep_Img']=addcslashes($row['wep_Img'],"s");
-		 return $row['wep_Img'];
+		 echo "$row['wep_Img']";
      }
 } else {
      echo "0 results";
